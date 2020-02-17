c4b8067c757298b76ccf703f6c5b6a845360115b
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-02-17 13:01:20 -0300

    Create class. Migrate class to database. Change class and replicate to database. Register class in django admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'created_date')
+    list_filter = ('user', 'event_date')
 
 # Register your models here.
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# Update database when the classes are modified.
+# python manage.py makemigrations app_name
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField(verbose_name='Event Date')
+    created_date = models.DateTimeField(auto_now=True)
+    # For multiple users. On delete user, delete every events.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Force table to be called by the name you choose
+    class Meta:
+        db_table = 'event'
+
+    def __str__(self):
+        return self.title
